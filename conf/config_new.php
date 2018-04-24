50065829883b3544b69d2ae453980ed4e4d8c836
tunwadee aongrit <tunwadeenarongrit@gmail.com>
2018-04-24 22:04:12 +0700

    111

---
+++ conf/config_new.php
@@ -0,0 +1,19 @@
+<?php
+$host = "localhost";
+$user_db = "id5511723_root";
+$pass = "12345678";
+$con = mysqli_connect($host, $user_db, $pass);
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+
+// ...some PHP code for database "my_db"...
+mysqli_select_db($con,"id5511723_webboard");
+mysqli_query($con,"SET NAMES UTF8");
+mysqli_query($con,"SET collection_connection='utf8'");
+mysqli_query($con,"SET character_set_results=utf8");
+mysqli_query($con,"SET character_set_client=utf8");
+mysqli_query($con,"SET character_set_connection=utf8");
+?>
